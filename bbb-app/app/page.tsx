c10f7ff65d2e43c02abc94c900b8c096fc2a25a1
aki-0517 <akki.pp.777@gmail.com>
2023-12-12 16:54:34 +0900

    add auth middleware

--- bbb-app/app/page.tsx
+++ bbb-app/app/page.tsx
@@ -1,10 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 dark-background">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex center-content">
-        <w3m-network-button />
-        <w3m-button />
+        {/* <w3m-network-button />
+        <w3m-button /> */}
+        <Link href="/auth">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-10 px-10 text-xl rounded-lg">
+          Sign In
+        </button>
+      </Link>
       </div>
     </main>
-  )
+  );
 }
--- bbb-app/providers/Providers.tsx
+++ bbb-app/providers/Providers.tsx
@@ -1,13 +1,18 @@
 "use client";
 import React from "react";
 import WagmiProvider from "./WagmiProvider";
+import NextAuthProvider from "./NextAuthProvider";
 
 type ProviderType = {
   children: React.ReactNode;
 };
 
 const Providers = ({ children }: ProviderType) => {
-  return <WagmiProvider>{children}</WagmiProvider>;
+  return (
+    <WagmiProvider>
+      <NextAuthProvider>{children}</NextAuthProvider>
+    </WagmiProvider>
+  );
 };
 
 export default Providers;
