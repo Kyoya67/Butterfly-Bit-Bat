f85dbe59a10123b174fa7fa49df3acd3c31fec34
aki-0517 <akki.pp.777@gmail.com>
2023-12-14 21:09:47 +0900

    型修正

--- bbb-app/app/api/auth/[...nextauth]/route.ts
+++ bbb-app/app/api/auth/[...nextauth]/route.ts
@@ -18,7 +18,7 @@ export const authOptions: AuthOptions = {
           const siwe = new SiweMessage(
             JSON.parse(credentials?.message || "{}")
           );
-          const nextAuthUrl = new URL(process.env.NEXTAUTH_URL);
+          const nextAuthUrl = new URL(process.env.NEXTAUTH_URL || "");
 
           const result = await siwe.verify({
             signature: credentials?.signature || "",
